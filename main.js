35f1b0154fae4138fe2b1fdfa8084b98c738b978
Brad Transtrum <Bradf3rdcc@hotmail.com>
2017-07-06 12:52:42 -0700

    added particles effect, and styling

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const electron = require('electron-compile')
+const electron = require('electron')
 // Module to control application life.
 const app = electron.app
 // Module to create native browser window.
